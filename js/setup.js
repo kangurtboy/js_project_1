454bcff2f48fca18b4cadddca571f7324d1019d5
kangurtboy <kangurt.boy@gmail.com>
2020-03-08 04:16:48 +0500

    создал основные масив данных

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,9 @@
 var setupBlock = document.querySelector('.setup');
 setupBlock.classList.remove('hidden');
-
+var names = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон']
+var surnames = ['да', 'Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 /* Задача
 В файле setup.js
