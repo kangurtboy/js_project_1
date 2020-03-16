fafdc03baa013ae664050bfc01495dc558f8a025
kangurtboy <kangurt.boy@gmail.com>
2020-03-17 00:50:55 +0500

    исправил ошибки

--- js/setup.js
+++ js/setup.js
@@ -26,13 +26,13 @@ var onMainSetupChanging = function(e) {
   var coatColorInput = setupBlock.querySelector('[name ="coat-color"]');
   var eyesColorInput = setupBlock.querySelector('[name="eyes-color"]');
   if (e.target.className.baseVal === "wizard-coat") {
-    coatColorInput.value = getRandomValue(coatColors);
+    coatColorInput.value = window.utils.getRandomValue(window.data.coatColors);
     e.target.style.fill = coatColorInput.value;
   } else if (e.target.className.baseVal === "wizard-eyes") {
-    eyesColorInput.value = getRandomValue(eyesColors);
+    eyesColorInput.value = window.utils.getRandomValue(window.data.eyesColors);
     e.target.style.fill = eyesColorInput.value;
   } else if (e.target.className === "setup-fireball") {
-    fireballInput.value = getRandomValue(fireballColors);
+    fireballInput.value = window.utils.getRandomValue(window.data.fireballColors);
     e.target.style.backgroundColor = fireballInput.value;
   }
 };
