7792e61ed7eedf8f690bf53fad409b420d16f83f
kangurtboy <kangurt.boy@gmail.com>
2020-03-30 18:54:24 +0500

    получил данные с сервера

--- js/backend.js
+++ js/backend.js
@@ -1,1 +1,22 @@
-console.log('work with server');
+(function () {
+	window.backend = {};
+	window.data = {};
+	window.data.vizards = [];
+	window.backend.load = function () {
+		//загрузка данных с сервера
+		var xhr = new XMLHttpRequest();
+		xhr.responseType = 'json';
+		xhr.timeout = 10000;
+		xhr.open('GET', 'https://js.dump.academy/code-and-magick/data');
+		xhr.addEventListener('load', function (e) {
+			onLoad(xhr.response);
+		});
+		xhr.send();
+	};
+	window.backend.load()
+	function onLoad(data) {
+		window.data = {};
+		window.data.vizards = data;
+		window.renderVizard();
+	};
+})()
