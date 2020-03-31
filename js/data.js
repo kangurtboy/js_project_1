e89f209d3e6a96bf599312725de873992ff3c0eb
kangurtboy <kangurt.boy@gmail.com>
2020-03-31 10:03:29 +0500

    исправление

--- js/data.js
+++ js/data.js
@@ -1,68 +1,28 @@
-// //данные персонажа
-// (function () {
-// 	window.data = {};
-//   window.data.names = [
-//     //имя персонажа
-//     "Иван",
-//     "Хуан",
-//     "Себастьян",
-//     "Мария",
-//     "Кристоф",
-//     "Виктор",
-//     "Юлия",
-//     "Люпита",
-//     "Вашингтон"
-//   ];
-//   window.data.surnames = [
-//     //фамилии персонажа
-//     "да",
-//     "Марья",
-//     "Верон",
-//     "Мирабелла",
-//     "Вальц",
-//     "Онопко",
-//     "Топольницкая",
-//     "Нионго",
-//     "Ирвинг"
-//   ];
-//   window.data.coatColors = [
-//     //цвет мантии
-//     "rgb(101, 137, 164)",
-//     "rgb(241, 43, 107)",
-//     "rgb(146, 100, 161)",
-//     "rgb(56, 159, 117)",
-//     "rgb(215, 210, 55)",
-//     "rgb(0, 0, 0)"
-//   ];
-//   window.data.eyesColors = [
-//     //цвет глаз
-//     "black",
-//     "red",
-//     "blue",
-//     "yellow",
-//     "green"
-//   ];
-//   window.data.fireballColors = [
-//     //цвет файрболла
-//     "#ee4830",
-//     "#30a8ee",
-//     "#5ce6c0",
-//     "#e848d5",
-//     "#e6e848"
-//   ];
-
-//   window.data.vizards = [
-//     //сам объект персонажа
-//   ];
-// 	for (var i = 0; i < 4; i++) {
-// 	  //заполнение персонажа рандомные данные
-//     window.data.vizards.push({
-//       name:
-//         window.utils.getRandomValue(window.data.names) +
-//         " " +
-//         window.utils.getRandomValue(window.data.surnames),
-//       coatColor: window.utils.getRandomValue(window.data.coatColors),
-//       eyesColor: window.utils.getRandomValue(window.data.eyesColors)
-//     });
-// 	}
-// })();
+//данные персонажа
+(function () {
+  window.data.coatColors = [
+    //цвет мантии
+    "rgb(101, 137, 164)",
+    "rgb(241, 43, 107)",
+    "rgb(146, 100, 161)",
+    "rgb(56, 159, 117)",
+    "rgb(215, 210, 55)",
+    "rgb(0, 0, 0)"
+  ];
+  window.data.eyesColors = [
+    //цвет глаз
+    "black",
+    "red",
+    "blue",
+    "yellow",
+    "green"
+  ];
+  window.data.fireballColors = [
+    //цвет файрболла
+    "#ee4830",
+    "#30a8ee",
+    "#5ce6c0",
+    "#e848d5",
+    "#e6e848"
+  ];
+})();
