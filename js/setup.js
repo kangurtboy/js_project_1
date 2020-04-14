589a4e1d89608d1420922ca6459db0c51dee3203
kangurtboy <kangurt.boy@gmail.com>
2020-04-14 16:39:22 +0500

    устранение дрбезга готово

--- js/setup.js
+++ js/setup.js
@@ -13,10 +13,10 @@
     eye: vizardTemplate.querySelector(".wizard-eyes"),
     fireballInput: setupBlock.querySelector('[name="fireball-color"]'),
     coatInput: setupBlock.querySelector('[name ="coat-color"]'),
-	eyesInput: setupBlock.querySelector('[name="eyes-color"]'),
+    eyesInput: setupBlock.querySelector('[name="eyes-color"]')
   };
-	function getRating(vizard) {
-	  //установка рейтинг похожести персонажей
+  function getRating(vizard) {
+    //установка рейтинг похожести персонажей
     vizard.rank = 0;
     if (
       vizard.colorCoat === mainVizard.coatInput.value &&
@@ -29,41 +29,45 @@
       vizard.rank = 1;
     }
   }
-	function sortingVizards(vizards) {
-	  //сортировка персонажей
+  function sortingVizards(vizards) {
+    //сортировка персонажей
     window.data.vizards.forEach(vizard => {
       getRating(vizard);
     });
     vizards.sort(function(a, b) {
-      return  b.rank - a.rank;
+      return b.rank - a.rank;
     });
-	};
-	window.renderVizard = function () {
-		vizardList.innerHTML = '';
-		sortingVizards(window.data.vizards);
-		//отрисовка персонажей
-		for (var i = 0; i < 4; i++) {
+  }
+  window.renderVizard = function() {
+    vizardList.innerHTML = "";
+    sortingVizards(window.data.vizards);
+    //отрисовка персонажей
+    for (var i = 0; i < 4; i++) {
       mainVizard.name.textContent = window.data.vizards[i].name;
       mainVizard.coat.style.fill = window.data.vizards[i].colorCoat;
       mainVizard.eye.style.fill = window.data.vizards[i].colorEyes;
       vizardList.appendChild(vizardTemplate.cloneNode(true));
     }
   };
-
   window.onMainSetupChanging = function(e) {
     //изменения главного персонажа;
     if (e.target.className.baseVal === "wizard-coat") {
       mainVizard.coatInput.value = window.utils.getRandomValue(
         window.data.coatColors
       );
-		e.target.style.fill = mainVizard.coatInput.value;
-		window.renderVizard();
+      e.target.style.fill = mainVizard.coatInput.value;
+      window.utils.debounce(
+		window.renderVizard
+	)
     } else if (e.target.className.baseVal === "wizard-eyes") {
       mainVizard.eyesInput.value = window.utils.getRandomValue(
         window.data.eyesColors
       );
 		e.target.style.fill = mainVizard.eyesInput.value;
-		window.renderVizard();
+		window.utils.debounce(
+			window.renderVizard
+		)
+     
     } else if (e.target.className === "setup-fireball") {
       mainVizard.fireballInput.value = window.utils.getRandomValue(
         window.data.fireballColors
--- js/utils.js
+++ js/utils.js
@@ -1,20 +1,28 @@
 //вспомогателные функции
 (function() {
   window.utils = {};
-	window.utils.getRandomValue = function (arr) {
-	  //возврашение случайного элемента массива
+  window.utils.getRandomValue = function(arr) {
+    //возврашение случайного элемента массива
     var randomValue = arr[Math.floor(Math.random() * arr.length)];
     return randomValue;
-	};
-	window.utils.getMaxValue = function (arr) {
-		//получение максмалное значение массива
-		var maxValue = arr[0];
-		for (let i = 0; i < arr.length; i++) {
-			if (arr[i] > maxValue) {
-				maxValue = arr[i];
-			}
-		};
-		maxValue = Math.floor(maxValue)
-		return maxValue;
+  };
+  window.utils.getMaxValue = function(arr) {
+    //получение максмалное значение массива
+    var maxValue = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > maxValue) {
+        maxValue = arr[i];
+      }
+    }
+    maxValue = Math.floor(maxValue);
+    return maxValue;
+  };
+  var timeout;
+	window.utils.debounce = function (func) {
+	//устранение дребезга
+	if (timeout) {
+		clearTimeout(timeout)
 	}
+	 timeout = setTimeout(func, 500);
+  };
 })();
